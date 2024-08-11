2548d6754a3e90a38a17d564c27cf515a7be4b5d
Jacky <jackylkx@hotmail.com>
2024-08-11 15:29:06 +0800

    add vercel .json

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,7 +9,8 @@ const PORT = process.env.PORT || 5000;
 //mongodb+srv://sa:*****@cluster0.rj3fd.mongodb.net/Property?retryWrites=true&w=majority&appName=Cluster0
 //mongodb://127.0.0.1:27017/Property
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/Property', {
+
+mongoose.connect(process.env.REACT_APP_MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
